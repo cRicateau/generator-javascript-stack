6d0de7a94c70a0f2d56822dc0227e0a9e873f732
cRicateau <clementrp@bamlab.fr>
2017-02-13 22:17:23 +0100

    Remove loopback-cli and add it as a prerequisite

--- generators/backend-generator/index.js
+++
@@ -1,7 +0,0 @@
-var Generator = require('yeoman-generator');
-
-module.exports = class extends Generator {
-  getBackend() {
-    return this.spawnCommandSync('npm', ['install', '-g', 'loopback-cli'])
-  }
-};
